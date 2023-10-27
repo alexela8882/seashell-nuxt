65d201fbc0554baa5cecbfe7dd6ddf537891e1d6
Renche Adams Dalida <renche.adams.16@gmail.com>
2023-10-27 18:42:21 +0800

    production build

---
+++ .nuxt/dist/server/pages/services.js
@@ -0,0 +1,113 @@
+exports.ids = [12];
+exports.modules = {
+
+/***/ 355:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/services.vue?vue&type=template&id=02d6677d&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('v-app',[_c('v-container',{attrs:{"fluid":"","fill-height":""}},[_c('v-layout',{attrs:{"align-center":"","justify-center":"","wrap":""}},[_c('v-flex',{attrs:{"xs12":"","sm8":"","md4":""}},[_c('h1',{staticClass:"text-h1 font-weight-black text-center"},[_c('v-icon',{staticClass:"text-h1 font-weight-black text-center"},[_vm._v("mdi-wrench")])],1),_vm._v(" "),_c('div',{staticClass:"mt-5 text-center"},[_c('p',{staticClass:"text-h5 font-weight-light"},[_vm._v("\n            This page is under construction.\n          ")]),_vm._v(" "),_c('v-btn',{attrs:{"color":"primary","nuxt-link":"","to":"/"}},[_vm._v("\n            Home\n          ")])],1)])],1)],1)],1)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/services.vue?vue&type=template&id=02d6677d&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/services.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var servicesvue_type_script_lang_js_ = ({
+  auth: false,
+  layout: 'landing',
+  head: {
+    title: 'Services'
+  }
+});
+// CONCATENATED MODULE: ./pages/services.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_servicesvue_type_script_lang_js_ = (servicesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(10);
+
+// EXTERNAL MODULE: ./node_modules/vuetify-loader/lib/runtime/installComponents.js
+var installComponents = __webpack_require__(9);
+var installComponents_default = /*#__PURE__*/__webpack_require__.n(installComponents);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VApp/VApp.js
+var VApp = __webpack_require__(242);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VBtn/VBtn.js
+var VBtn = __webpack_require__(228);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VGrid/VContainer.js
+var VContainer = __webpack_require__(243);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VGrid/VFlex.js
+var VFlex = __webpack_require__(244);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VIcon/VIcon.js
+var VIcon = __webpack_require__(81);
+
+// EXTERNAL MODULE: ./node_modules/vuetify/lib/components/VGrid/VLayout.js
+var VLayout = __webpack_require__(245);
+
+// CONCATENATED MODULE: ./pages/services.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_servicesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "2809747a"
+  
+)
+
+/* harmony default export */ var services = __webpack_exports__["default"] = (component.exports);
+
+/* vuetify-loader */
+
+
+
+
+
+
+
+installComponents_default()(component, {VApp: VApp["a" /* default */],VBtn: VBtn["a" /* default */],VContainer: VContainer["a" /* default */],VFlex: VFlex["a" /* default */],VIcon: VIcon["a" /* default */],VLayout: VLayout["a" /* default */]})
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=services.js.map
